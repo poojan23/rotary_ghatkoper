78b2772cdc15d0c01b836e8e3df6b65603c7aead
Ketanatdi <ksadigale@atdi.in>
2019-06-27 14:10:52 +0530

    change in config file

--- sadmin/config.php
+++ sadmin/config.php
@@ -1,21 +1,21 @@
 <?php
 # HTTP
-define('HTTP_SERVER', 'http://localhost/rotary/sadmin/');
-define('HTTP_ADMIN', 'http://localhost/rotary/admin/');
-define('HTTP_CATALOG', 'http://localhost/rotary/');
+define('HTTP_SERVER', 'http://localhost/rotary_ghatkoper/sadmin/');
+define('HTTP_ADMIN', 'http://localhost/rotary_ghatkoper/admin/');
+define('HTTP_CATALOG', 'http://localhost/rotary_ghatkoper/');
 
 # HTTPS
-define('HTTPS_SERVER', 'http://localhost/rotary/sadmin/');
-define('HTTPS_ADMIN', 'http://localhost/rotary/admin/');
-define('HTTPS_CATALOG', 'http://localhost/rotary/');
+define('HTTPS_SERVER', 'http://localhost/rotary_ghatkoper/sadmin/');
+define('HTTPS_ADMIN', 'http://localhost/rotary_ghatkoper/admin/');
+define('HTTPS_CATALOG', 'http://localhost/rotary_ghatkoper/');
 
 # DIR
-define('DIR_APPLICATION', 'C:/ATDI/LMS/htdocs/rotary/sadmin/');
-define('DIR_SYSTEM', 'C:/ATDI/LMS/htdocs/rotary/system/');
-define('DIR_IMAGE', 'C:/ATDI/LMS/htdocs/rotary/image/');
+define('DIR_APPLICATION', 'C:/ATDI/LMS/htdocs/rotary_ghatkoper/sadmin/');
+define('DIR_SYSTEM', 'C:/ATDI/LMS/htdocs/rotary_ghatkoper/system/');
+define('DIR_IMAGE', 'C:/ATDI/LMS/htdocs/rotary_ghatkoper/image/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_CATALOG', 'C:/ATDI/LMS/htdocs/rotary/template/');
-define('DIR_ADMIN', 'C:/ATDI/LMS/htdocs/rotary/admin/');
+define('DIR_CATALOG', 'C:/ATDI/LMS/htdocs/rotary_ghatkoper/template/');
+define('DIR_ADMIN', 'C:/ATDI/LMS/htdocs/rotary_ghatkoper/admin/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/html/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -31,7 +31,7 @@ define('DB_DRIVER', 'pdo');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'rotary');
+define('DB_DATABASE', 'rotary_gk');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'pt_');
 
